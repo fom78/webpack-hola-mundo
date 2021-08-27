5c98f5ac7560b1e44dd5b14ea730e8ad6e8564d6
Fernando <fom78a@gmail.com>
2021-08-27 19:30:24 -0300

    inicial

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import style from "./style.css"
+
+const app = document.getElementById("app")
+const div = document.createElement("div");
+
+div.textContent = "Hola mundo desde webpack.";
+app.appendChild(div);
+
